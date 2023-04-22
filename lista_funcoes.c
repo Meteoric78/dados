3bfb94fe805087220a98714efecdafa0d46d103c
Meteoric78 <66694522+Meteoric78@users.noreply.github.com>
2023-04-22 20:55:54 -0300

    Add files via upload

--- lista_funcoes.c
+++ lista_funcoes.c
@@ -6,6 +6,11 @@ typedef struct no{
 	struct no *prox;
 }No;
 
+typedef struct desc{
+	int qtd;
+	struct no *ini, *fim, *maior;
+}Desc;
+
 No * criar_no(int valor){
 	No *novo = (No *)malloc(sizeof(No));
 	novo->dado = valor;
@@ -13,27 +18,65 @@ No * criar_no(int valor){
 	return novo;
 }
 
-void linkar_nos(No *no1, No *no2){
-	no1->prox = no2;
-}
-
-void mostrar_no(No *nodezinho){
-	printf("%d\n", nodezinho->dado);
-}
-
-void mostrar_nos(No *nodezinho){
+int def_maior(Desc *d){
 	No *p;
-	for (p=nodezinho; p!=NULL; p=p->prox){
-		printf("%d\n", p->dado);
+	for(p=d->ini; p!=NULL; p=p->prox){
+		if (p->dado > d->maior->dado)
+			d->maior = p;
+	}
+	return 0;
+}
+
+int inserir_fim(Desc *d, int valor){
+	No *novo = criar_no(valor);
+	if (d->fim == NULL){
+		d->ini = novo;
+		d->fim = novo;
+		d->maior = d->fim;
+	}else{
+		d->fim->prox = novo;
+		d->fim = novo;
+		def_maior(d);
+	}
+	d->qtd++;
+	return 0;	
+}
+
+
+int mostrar_desc(Desc *d){
+	printf("qtd = %d\n", d->qtd);
+	printf("ini = %d\n", d->ini->dado);
+	printf("fim = %d\n", d->fim->dado);
+	printf("maior = %d\n", d->maior->dado);
+	return 0;
+}
+
+void mostrar_lista(Desc *d){
+	if (d->ini == NULL)
+		printf("Lista vazia.\n");
+	else{
+		No *p;
+		for (p=d->ini; p!=NULL; p=p->prox){
+			printf("%i\n", p->dado);
+		}
 	}
 }
 
+
 int main() {
-	No *node1 = criar_no(7);
-	mostrar_no(node1);
-	No *node2 = criar_no(9);
-	mostrar_no(node2);
-	linkar_nos(node1, node2);
-	mostrar_nos(node1);
+	Desc d;
+	d.qtd = 0;
+	d.ini = NULL;
+	d.fim = NULL;
+	d.maior = NULL;
+
+	inserir_fim(&d, 10);
+	inserir_fim(&d, 20);
+	inserir_fim(&d, 30);
+	inserir_fim(&d, 100);
+
+	mostrar_desc(&d);
+
+	mostrar_lista(&d);
 
 }
